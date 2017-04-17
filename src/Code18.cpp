f3a1a7b4dd8870be15ec858bb79d36956ef9c881
monkeyligang <646109005@qq.com>
2017-04-18 00:48:49 +0800

    二叉树的镜像
    操作给定的二叉树，将其变换为源二叉树的镜像。

---
+++ src/Code18.cpp
@@ -0,0 +1,40 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+//递归左子树和右子树交换
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+       if(pRoot == NULL)
+           return;
+       else{
+           TreeNode *node = pRoot->left;
+           pRoot->left = pRoot->right;
+           pRoot->right=node;
+           Mirror(pRoot->left);
+           Mirror(pRoot->right);
+       }
+    }
+};
+
+
+操作给定的二叉树，将其变换为源二叉树的镜像。 
+输入描述:
+二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+    	11 9 7  5
